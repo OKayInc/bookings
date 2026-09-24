9a7c63cd9a0e3669550a16488574595422ad4781
Luis Daniel Lucio Quiroz <luis.daniel.lucio@gmail.com>
2026-09-24 17:13:26 -0400

    Delete bootstrap/config directory

--- bootstrap/config/booking.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'public_hold_ttl_minutes' => (int) env('BOOKING_PUBLIC_HOLD_TTL_MINUTES', 15),
-    'email_verification_ttl_hours' => (int) env('BOOKING_EMAIL_VERIFICATION_TTL_HOURS', 24),
-    'schedule_proposal_default_ttl_hours' => (int) env('BOOKING_SCHEDULE_PROPOSAL_DEFAULT_TTL_HOURS', 24),
-    'schedule_proposal_max_ttl_hours' => (int) env('BOOKING_SCHEDULE_PROPOSAL_MAX_TTL_HOURS', 168),
-    'reference_length' => 12,
-];
--- bootstrap/config/equipment.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'max_inventory_quantity' => 100000,
-    'max_bundle_tiers' => 20,
-];
