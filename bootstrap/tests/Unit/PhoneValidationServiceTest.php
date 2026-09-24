314a4928b99931979430f2b2881a156445f0413f
Luis Daniel Lucio Quiroz <luis.daniel.lucio@gmail.com>
2026-09-24 17:17:07 -0400

    Delete bootstrap/tests directory

--- bootstrap/tests/Unit/PhoneValidationServiceTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Tests\Unit;
-use App\Domain\Questionnaires\PhoneValidationService;
-use Tests\TestCase;
-class PhoneValidationServiceTest extends TestCase {
- public function test_canadian_number_is_normalized_to_e164(): void { $this->assertSame('+16135551234',app(PhoneValidationService::class)->validateAndNormalize('(613) 555-1234','CA')); }
-}
